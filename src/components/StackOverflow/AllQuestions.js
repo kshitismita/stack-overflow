ee631bab34fe69f24781dfb763142d47a8c6337e
Aditya Pradhan <kshitismita.panda@gmail.com>
2024-11-20 08:55:55 +0530

    fourth commit : stylizing & other stuff

---
+++ src/components/StackOverflow/AllQuestions.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AllQuestions() {
+  return (
+    <div>
+      All Questions
+    </div>
+  )
+}
+
+export default AllQuestions
--- src/components/StackOverflow/Main.js
+++ src/components/StackOverflow/Main.js
@@ -1,8 +1,38 @@
 import React from 'react'
+import AllQuestions from './AllQuestions'
+import './css/Main.css'
 
 function Main() {
   return (
-    <div>Hello Main Component</div>
+    <div className='main'>
+        <div className='main-container'>
+            <div className='mian-top'>
+                <h2>Top Questions</h2>
+                <div className='ask-question'>Ask Questions</div>
+            </div>
+            <div className='main-dec'>
+                <p>All Questions Stat</p>
+                <div className='main-filter'>
+                    <div className='main-tabs'>
+                        <div className='main-tab'>
+                            <p>Option 1</p>
+                        </div>
+                        <div className='main-tab'>
+                            <p>Option 2</p>
+                        </div>
+                        <div className='main-tab'>
+                            <p>Option 3</p>
+                        </div>
+                    </div>
+                    </div>
+                    <div className='questions'>
+                        <div className='question'>
+                            <AllQuestions />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
   )
 }
 
